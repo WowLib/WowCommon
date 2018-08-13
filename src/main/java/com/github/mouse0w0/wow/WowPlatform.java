fcfc814c33a5bccb820f4794805601fb16f2337c
Mouse <937279156@qq.com>
2018-08-13 20:47:47 +0800

    Update

---
+++ src/main/java/com/github/mouse0w0/wow/WowPlatform.java
@@ -0,0 +1,6 @@
+package com.github.mouse0w0.wow;
+
+public class WowPlatform {
+    public static final String VERSION = "1.0.0";
+    public static final int INTERNAL_VERSION = 1;
+}
---
+++ src/main/java/com/github/mouse0w0/wow/network/packet/common/VerificationPacket.java
@@ -0,0 +1,31 @@
+package com.github.mouse0w0.wow.network.packet.common;
+
+import com.github.mouse0w0.wow.network.Connection;
+import com.github.mouse0w0.wow.network.Packet;
+import com.github.mouse0w0.wow.network.PacketHandler;
+import io.netty.buffer.ByteBuf;
+
+public class VerificationPacket implements Packet {
+
+    private int internalVersion;
+
+    public VerificationPacket(){}
+
+    public VerificationPacket(int internalVersion){
+        this.internalVersion = internalVersion;
+    }
+
+    public int getInternalVersion() {
+        return internalVersion;
+    }
+
+    @Override
+    public void read(ByteBuf buffer) {
+        internalVersion = buffer.readInt();
+    }
+
+    @Override
+    public void write(ByteBuf buffer) {
+        buffer.writeInt(internalVersion);
+    }
+}
