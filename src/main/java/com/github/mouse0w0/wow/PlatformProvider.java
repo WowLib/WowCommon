9a4caec66a89acb84aa7a35081af45e97c82562d
Mouse <937279156@qq.com>
2018-10-03 17:56:48 +0800

    Update

---
+++ src/main/java/com/github/mouse0w0/wow/PlatformProvider.java
@@ -0,0 +1,8 @@
+package com.github.mouse0w0.wow;
+
+import com.github.mouse0w0.wow.network.NetworkManager;
+
+public interface PlatformProvider {
+
+    NetworkManager getNetwork();
+}
--- src/main/java/com/github/mouse0w0/wow/WowPlatform.java
+++ src/main/java/com/github/mouse0w0/wow/WowPlatform.java
@@ -1,9 +1,39 @@
 package com.github.mouse0w0.wow;
 
-public class WowPlatform {
-    public static final String NAME = "Wow";
-    public static final String VERSION = "1.0.0";
-    public static final int INTERNAL_VERSION = 1;
+import com.github.mouse0w0.wow.network.NetworkManager;
 
-    public static final String NETWORK_CHANNEL_NAME = "Wow";
+public class WowPlatform {
+    private static final String NAME = "Wow";
+    private static final String VERSION = "1.0.0";
+    private static final int INTERNAL_VERSION = 1;
+
+    private static final String NETWORK_CHANNEL_NAME = "Wow";
+
+    private static PlatformProvider platformProvider;
+
+    public static String getName() {
+        return NAME;
+    }
+
+    public static String getVersion() {
+        return VERSION;
+    }
+
+    public static int getInternalVersion() {
+        return INTERNAL_VERSION;
+    }
+
+    public static String getNetworkChannelName() {
+        return NETWORK_CHANNEL_NAME;
+    }
+
+    public static NetworkManager getNetwork() {
+        return platformProvider.getNetwork();
+    }
+
+    public static void setPlatformProvider(PlatformProvider platformProvider) {
+        if(platformProvider != null)
+            throw new IllegalStateException("Wow platform has been initialized.");
+        WowPlatform.platformProvider = platformProvider;
+    }
 }
