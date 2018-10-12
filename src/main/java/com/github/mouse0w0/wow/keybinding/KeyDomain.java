1a13d7e097f843bd36b42f9717060f76e2425546
Mouse <937279156@qq.com>
2018-10-12 19:59:53 +0800

    Update KeyBinding

--- src/main/java/com/github/mouse0w0/wow/keybinding/KeyDomain.java
+++ src/main/java/com/github/mouse0w0/wow/keybinding/KeyDomain.java
@@ -1,7 +1,24 @@
 package com.github.mouse0w0.wow.keybinding;
 
 public enum KeyDomain {
-    UNIVERSAL,
-    GUI,
-    IN_GAME
+    UNIVERSAL {
+        @Override
+        public boolean isConflicts(KeyDomain other) {
+            return true;
+        }
+    },
+    GUI {
+        @Override
+        public boolean isConflicts(KeyDomain other) {
+            return other == this || other == UNIVERSAL;
+        }
+    },
+    IN_GAME {
+        @Override
+        public boolean isConflicts(KeyDomain other) {
+            return other == this || other == UNIVERSAL;
+        }
+    };
+
+    public abstract boolean isConflicts(KeyDomain other);
 }
