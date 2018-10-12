ee07609600a1131e5677f42b3b77090d0b7c7b0e
Mouse <937279156@qq.com>
2018-10-12 22:31:03 +0800

    Fixed RegisterKeyBindingPacket

--- src/main/java/com/github/mouse0w0/wow/network/packet/server/RegisterKeyBindingPacket.java
+++ src/main/java/com/github/mouse0w0/wow/network/packet/server/RegisterKeyBindingPacket.java
@@ -1,67 +1,49 @@
 package com.github.mouse0w0.wow.network.packet.server;
 
-import com.github.mouse0w0.wow.keybinding.Key;
-import com.github.mouse0w0.wow.keybinding.KeyBinding;
-import com.github.mouse0w0.wow.keybinding.KeyDomain;
-import com.github.mouse0w0.wow.keybinding.KeyModifier;
+import com.github.mouse0w0.wow.keybinding.*;
 import com.github.mouse0w0.wow.network.Packet;
+import com.github.mouse0w0.wow.registry.Registry;
 import com.github.mouse0w0.wow.util.BufUtils;
+import com.github.mouse0w0.wow.util.GsonUtils;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 import io.netty.buffer.ByteBuf;
 
 public class RegisterKeyBindingPacket implements Packet {
 
-    private int keyBindingId;
-    private Key defaultKey;
-    private KeyModifier defaultModifier;
-    private KeyDomain domain;
-    private String displayName;
+    private JsonArray json;
 
     public RegisterKeyBindingPacket() {
     }
 
-    public RegisterKeyBindingPacket(int keyBindingId, KeyBinding keyBinding) {
-        this.keyBindingId = keyBindingId;
-        defaultKey = keyBinding.getDefaultKey();
-        defaultModifier = keyBinding.getDefaultModifier();
-        domain = keyBinding.getDomain();
-        displayName = keyBinding.getDisplayName();
+    public RegisterKeyBindingPacket(Registry<ServerKeyBinding> serverKeyBindings) {
+        json = new JsonArray();
+        for (ServerKeyBinding serverKeyBinding : serverKeyBindings.getValues()) {
+            json.add(toJsonObject(serverKeyBindings.getId(serverKeyBinding), serverKeyBinding));
+        }
     }
 
-    public int getKeyBindingId() {
-        return keyBindingId;
+    private JsonObject toJsonObject(Integer id, ServerKeyBinding serverKeyBinding) {
+        JsonObject json = new JsonObject();
+        json.addProperty("id", id);
+        json.addProperty("key", serverKeyBinding.getDefaultKey().getCode());
+        json.addProperty("mod", serverKeyBinding.getDefaultModifier().ordinal());
+        json.addProperty("domain", serverKeyBinding.getDomain().ordinal());
+        json.addProperty("display", serverKeyBinding.getDisplayName());
+        return json;
     }
 
-    public Key getDefaultKey() {
-        return defaultKey;
-    }
-
-    public KeyDomain getDomain() {
-        return domain;
-    }
-
-    public KeyModifier getDefaultModifier() {
-        return defaultModifier;
-    }
-
-    public String getDisplayName() {
-        return displayName;
+    public JsonArray getJson() {
+        return json;
     }
 
     @Override
     public void read(ByteBuf buffer) {
-        keyBindingId = BufUtils.readVarInt(buffer);
-        defaultKey = BufUtils.readEnum(buffer, Key.class);
-        defaultModifier = BufUtils.readEnum(buffer, KeyModifier.class);
-        domain = BufUtils.readEnum(buffer, KeyDomain.class);
-        displayName = BufUtils.readString(buffer);
+        json = GsonUtils.JSON_PARSER.parse(BufUtils.readString(buffer)).getAsJsonArray();
     }
 
     @Override
     public void write(ByteBuf buffer) {
-        BufUtils.writeVarInt(buffer, keyBindingId);
-        BufUtils.writeEnum(buffer, defaultKey);
-        BufUtils.writeEnum(buffer, defaultModifier);
-        BufUtils.writeEnum(buffer, domain);
-        BufUtils.writeString(buffer, displayName);
+        BufUtils.writeString(buffer, json.toString());
     }
 }
---
+++ src/main/java/com/github/mouse0w0/wow/util/GsonUtils.java
@@ -0,0 +1,8 @@
+package com.github.mouse0w0.wow.util;
+
+import com.google.gson.JsonParser;
+
+public class GsonUtils {
+
+    public static final JsonParser JSON_PARSER = new JsonParser();
+}
